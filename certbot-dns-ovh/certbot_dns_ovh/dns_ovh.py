aa423007167d85b59d0df7dacedf814726154c4e
Romain Beuque <romain.beuque@gmail.com>
2017-08-15 22:01:58 +0200

    Modification for OVH API

--- certbot-dns-ovh/certbot_dns_ovh/dns_ovh.py
+++ certbot-dns-ovh/certbot_dns_ovh/dns_ovh.py
@@ -1,7 +1,9 @@
-"""DNS Authenticator for Cloudflare."""
+"""DNS Authenticator for OVH."""
 import logging
 
-import CloudFlare
+import ovh
+import ovh.exceptions
+
 import zope.interface
 
 from certbot import errors
@@ -10,18 +12,16 @@ from certbot.plugins import dns_common
 
 logger = logging.getLogger(__name__)
 
-ACCOUNT_URL = 'https://www.cloudflare.com/a/account/my-account'
-
 
 @zope.interface.implementer(interfaces.IAuthenticator)
 @zope.interface.provider(interfaces.IPluginFactory)
 class Authenticator(dns_common.DNSAuthenticator):
-    """DNS Authenticator for Cloudflare
+    """DNS Authenticator for OVH
 
-    This Authenticator uses the Cloudflare API to fulfill a dns-01 challenge.
+    This Authenticator uses the OVH API to fulfill a dns-01 challenge.
     """
 
-    description = ('Obtain certificates using a DNS TXT record (if you are using Cloudflare for '
+    description = ('Obtain certificates using a DNS TXT record (if you are using OVH for '
                    'DNS).')
     ttl = 120
 
@@ -32,39 +32,45 @@ class Authenticator(dns_common.DNSAuthenticator):
     @classmethod
     def add_parser_arguments(cls, add):  # pylint: disable=arguments-differ
         super(Authenticator, cls).add_parser_arguments(add)
-        add('credentials', help='Cloudflare credentials INI file.')
+        add('credentials', help='OVH credentials INI file.')
 
     def more_info(self):  # pylint: disable=missing-docstring,no-self-use
         return 'This plugin configures a DNS TXT record to respond to a dns-01 challenge using ' + \
-               'the Cloudflare API.'
+               'the OVH API.'
 
     def _setup_credentials(self):
         self.credentials = self._configure_credentials(
             'credentials',
-            'Cloudflare credentials INI file',
+            'OVH credentials INI file',
             {
-                'email': 'email address associated with Cloudflare account',
-                'api-key': 'API key for Cloudflare account, obtained from {0}'.format(ACCOUNT_URL)
+                'application_key': 'Application Key',
+                'application_secret': 'Application Secret',
+                'consumer_key': 'Consumer secret associated to your OVH account',
             }
         )
 
     def _perform(self, domain, validation_name, validation):
-        self._get_cloudflare_client().add_txt_record(domain, validation_name, validation, self.ttl)
+        self._get_ovh_client().add_txt_record(domain, validation_name, validation, self.ttl)
 
     def _cleanup(self, domain, validation_name, validation):
-        self._get_cloudflare_client().del_txt_record(domain, validation_name, validation)
+        self._get_ovh_client().del_txt_record(domain, validation_name, validation)
 
-    def _get_cloudflare_client(self):
-        return _CloudflareClient(self.credentials.conf('email'), self.credentials.conf('api-key'))
+    def _get_ovh_client(self):
+        return _OVHClient(self.credentials.conf('application_key'), self.credentials.conf('application_secret'), self.credentials.conf('consumer_key'))
 
 
-class _CloudflareClient(object):
+class _OVHClient(object):
     """
-    Encapsulates all communication with the Cloudflare API.
+    Encapsulates all communication with the OVH API.
     """
 
-    def __init__(self, email, api_key):
-        self.cf = CloudFlare.CloudFlare(email, api_key)
+    def __init__(self, application_key, application_secret, consumer_key):
+        self.client = ovh.Client(
+            endpoint='ovh-eu', # For now, only EU region handles DNS
+            application_key=application_key,
+            application_secret=application_secret,
+            consumer_key=consumer_key
+        )
 
     def add_txt_record(self, domain, record_name, record_content, record_ttl):
         """
@@ -77,22 +83,21 @@ class _CloudflareClient(object):
         :raises certbot.errors.PluginError: if an error occurs communicating with the Cloudflare API
         """
 
-        zone_id = self._find_zone_id(domain)
+        zone_name = self._find_zone_for_domain(domain)
 
-        data = {'type': 'TXT',
-                'name': record_name,
-                'content': record_content,
-                'ttl': record_ttl}
+        dns_record = self.client.post("/domain/zone/{0}/record".format(zone_name), fieldType='TXT',
+                                 subDomain=record_name, target=record_content, ttl=record_ttl)
+        record_id = dns_record['id']
+        logger.debug("Added TXT record on OVH DNS Zone, ID is %d", record_id)
 
-        try:
-            logger.debug('Attempting to add record to zone %s: %s', zone_id, data)
-            self.cf.zones.dns_records.post(zone_id, data=data)  # zones | pylint: disable=no-member
-        except CloudFlare.exceptions.CloudFlareAPIError as e:
-            logger.error('Encountered CloudFlareAPIError adding TXT record: %d %s', e, e)
-            raise errors.PluginError('Error communicating with the Cloudflare API: {0}'.format(e))
+        self._refresh_dns_zone(zone_name)
 
-        record_id = self._find_txt_record_id(zone_id, record_name, record_content)
-        logger.debug('Successfully added TXT record with record_id: %s', record_id)
+    def _refresh_dns_zone(self, zone_name):
+        self.client.post('/domain/zone/{0}/refresh'.format(zone_name))
+        logger.info("Zone have been refreshed on OVH side")
+
+        soa = self.client.get('/domain/zone/{0}/soa'.format(zone_name))
+        logger.debug("SOA serial for DNS Zone is now %s", soa['serial'])
 
     def del_txt_record(self, domain, record_name, record_content):
         """
@@ -108,27 +113,26 @@ class _CloudflareClient(object):
         :param str record_content: The record content (typically the challenge validation).
         """
 
-        try:
-            zone_id = self._find_zone_id(domain)
-        except errors.PluginError as e:
-            logger.debug('Encountered error finding zone_id during deletion: %s', e)
+        zone_name = self._find_zone_for_domain(domain)
+
+        records = client.get("/domain/zone/{0}/record".format(zone_name), fieldType='TXT', subDomain=record_name)
+
+        if len(records) <= 0:
+            logger.debug("No record found, nothing to clean up")
             return
+        record_id = records[0]
 
-        if zone_id:
-            record_id = self._find_txt_record_id(zone_id, record_name, record_content)
-            if record_id:
-                try:
-                    # zones | pylint: disable=no-member
-                    self.cf.zones.dns_records.delete(zone_id, record_id)
-                    logger.debug('Successfully deleted TXT record.')
-                except CloudFlare.exceptions.CloudFlareAPIError as e:
-                    logger.warn('Encountered CloudFlareAPIError deleting TXT record: %s', e)
-            else:
-                logger.debug('TXT record not found; no cleanup needed.')
-        else:
-            logger.debug('Zone not found; no cleanup needed.')
+        if len(records) > 1:
+            # Too many records, will delete the last one we added.
+            record_id = records[-1]
 
-    def _find_zone_id(self, domain):
+        logger.debug("Deleting TXT record on DNS Zone (name=%s, id=%d)", record_name, record_id)
+
+        client.delete('/domain/zone/{0}/record/{1}'.format(zone_name, record_id))
+
+        self._refresh_dns_zone(zone_name)
+
+    def _find_zone_for_domain(self, domain):
         """
         Find the zone_id for a given domain.
 
@@ -141,59 +145,15 @@ class _CloudflareClient(object):
         zone_name_guesses = dns_common.base_domain_name_guesses(domain)
 
         for zone_name in zone_name_guesses:
-            params = {'name': zone_name,
-                      'per_page': 1}
-
             try:
-                zones = self.cf.zones.get(params=params)  # zones | pylint: disable=no-member
-            except CloudFlare.exceptions.CloudFlareAPIError as e:
-                code = int(e)
-                hint = None
+                self.client.get("/domain/zone/{0}".format(zone_name))
+                logger.debug('Found zone %s against OVH API', zone_name)
 
-                if code == 6003:
-                    hint = 'Did you copy your entire API key?'
-                elif code == 9103:
-                    hint = 'Did you enter the correct email address?'
+                # TODO: check here if we are handling well zone declared on a sub-domain,
+                # so that record_name correspond to the correct one.
+                return zone_name
+            except ovh.exceptions.ResourceNotFoundError as exception_catched:
+                continue
 
-                raise errors.PluginError('Error determining zone_id: {0} {1}. Please confirm that '
-                                         'you have supplied valid Cloudflare API credentials.{2}'
-                                         .format(code, e, ' ({0})'.format(hint) if hint else ''))
-
-            if len(zones) > 0:
-                zone_id = zones[0]['id']
-                logger.debug('Found zone_id of %s for %s using name %s', zone_id, domain, zone_name)
-                return zone_id
-
-        raise errors.PluginError('Unable to determine zone_id for {0} using zone names: {1}. '
-                                 'Please confirm that the domain name has been entered correctly '
-                                 'and is already associated with the supplied Cloudflare account.'
-                                 .format(domain, zone_name_guesses))
-
-    def _find_txt_record_id(self, zone_id, record_name, record_content):
-        """
-        Find the record_id for a TXT record with the given name and content.
-
-        :param str zone_id: The zone_id which contains the record.
-        :param str record_name: The record name (typically beginning with '_acme-challenge.').
-        :param str record_content: The record content (typically the challenge validation).
-        :returns: The record_id, if found.
-        :rtype: str
-        """
-
-        params = {'type': 'TXT',
-                  'name': record_name,
-                  'content': record_content,
-                  'per_page': 1}
-        try:
-            # zones | pylint: disable=no-member
-            records = self.cf.zones.dns_records.get(zone_id, params=params)
-        except CloudFlare.exceptions.CloudFlareAPIError as e:
-            logger.debug('Encountered CloudFlareAPIError getting TXT record_id: %s', e)
-            records = []
-
-        if len(records) > 0:
-            # Cleanup is returning the system to the state we found it. If, for some reason,
-            # there are multiple matching records, we only delete one because we only added one.
-            return records[0]['id']
-        else:
-            logger.debug('Unable to find TXT record.')
+        raise errors.PluginError('Unable to find DNS Zone for domain {0} in your OVH Account'
+                                 .format(domain))
